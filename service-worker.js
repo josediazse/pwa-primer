974902e7a58f2cb0feb2c6a87485def0ae0cd92e
josediazse <jediazse@gmail.com>
2017-07-23 23:49:25 -0400

    Adding 'Cache then network' strategy to fetch weather data.

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+var dataCacheName = 'weatherData-v1'
 var cacheName = 'weatherPWA-step-6-1'
 var filesToCache = [
     '/',
@@ -19,22 +20,22 @@ var filesToCache = [
     '/images/wind.png'
 ]
 
-self.addEventListener('install', function(e){
+self.addEventListener('install', function (e) {
     console.log('[ServiceWorker] Install')
     e.waitUntil(
-        caches.open(cacheName).then(function(cache){
+        caches.open(cacheName).then(function (cache) {
             console.log('[Service Worker] Caching app shell')
             return cache.addAll(filesToCache)
         })
     )
 })
 
-self.addEventListener('activate', function(e){
+self.addEventListener('activate', function (e) {
     console.log('[ServiceWorker] Activate')
     e.waitUntil(
-        caches.keys().then(function(keyList){
-            return Promise.all(keyList.map(function(key){
-                if(key !== cacheName){
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName && key !== dataCacheName) {
                     console.log('[ServiceWorker] Removing old cache', key)
                     return caches.delete(key)
                 }
@@ -44,11 +45,23 @@ self.addEventListener('activate', function(e){
     return self.clients.claim();
 })
 
-self.addEventListener('fetch', function(e){
+self.addEventListener('fetch', function (e) {
     console.log('[ServiceWorker] Fetch', e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function(response){
-            return response || fetch(e.request)
-        })
-    )
+    var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+    if (e.request.url.indexOf(dataUrl) > -1) {
+        e.respondWith(
+            caches.open(dataCacheName).then(function (cache) {
+                return fetch(e.request).then(function (response) {
+                    cache.put(e.request.url, response.clone());
+                    return response;
+                })
+            })
+        )
+    } else {
+        e.respondWith(
+            caches.match(e.request).then(function (response) {
+                return response || fetch(e.request)
+            })
+        )
+    }
 })
